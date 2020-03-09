3256cd8c67d681e44e9062e84480bedde6ca20a0
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-03-09 19:23:32 -0300

    Códigos da aula 04 - Exercícios com Programação Estruturada em PHP

---
+++ Aula4_PHP_Estruturado/06.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Fonte: https://www.php.net/manual/en/function.key.php
+*/
+$array = array(
+    'fruit1' => 'apple',
+    'fruit2' => 'orange',
+    'fruit3' => 'grape',
+    'fruit4' => 'apple',
+    'fruit5' => 'apple');
+
+// this cycle echoes all associative array
+// key where value equals "apple"
+while ($fruit_name = current($array)) {
+    if ($fruit_name == 'apple') {
+        echo key($array).'<br />';
+    }
+    next($array);
+}
+?>
